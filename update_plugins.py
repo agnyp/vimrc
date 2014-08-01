d55d9991fef7a6eefb82a3649c636add8b509bd5
Andreas Gnyp <agnyp@arri.de>
2014-08-01 18:38:01 +0200

    add supertab and a macro
    macro is for removing indentation from line

--- update_plugins.py
+++ update_plugins.py
@@ -39,6 +39,7 @@ syntastic https://github.com/scrooloose/syntastic
 vim-repeat https://github.com/tpope/vim-repeat
 vim-commentary https://github.com/tpope/vim-commentary
 vim-golang https://github.com/jnwhiteh/vim-golang
+supertab https://github.com/ervandew/supertab
 """.strip()
 
 GITHUB_ZIP = '%s/archive/master.zip'
